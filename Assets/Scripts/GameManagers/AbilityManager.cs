175207f8789485062d4a10b1c26d63fa969feeb3
LemonMaster66 <539993@chrome.escg.ac.uk>
2024-07-04 00:59:39 +0100

    Get 3 Random Abilities
    + GetRandomAbilities() / Display
    
    # Press F4 to get 3 Random Abilities

--- Assets/Scripts/GameManagers/AbilityManager.cs
+++ Assets/Scripts/GameManagers/AbilityManager.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using PalexUtilities;
 using UnityEngine;
 
 public class AbilityManager : MonoBehaviour
@@ -11,21 +14,16 @@ public class AbilityManager : MonoBehaviour
 
     void Awake()
     {
-        //AddAbilites();
+        AddAbilites();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        if(Input.GetKeyDown(KeyCode.Tab)) ApplyAbility(abilities[1]);
+        if(Input.GetKeyDown(KeyCode.F4))
         {
-            AddAbilites();
-            ApplyAbility(abilities[1]);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            AddAbilites();
-            ApplyAbility(abilities[0]);
+            Tools.ClearLogConsole();
+            DisplayRandomAbilities();
         }
     }
 
@@ -36,18 +34,33 @@ public class AbilityManager : MonoBehaviour
         gunManager = FindAnyObjectByType<GunManager>();
         ActiveGun = gunManager.GetGun(gunManager.ActiveGun);
 
-        abilities.Add(new Ability("Damage Boost", "Increases damage by 10",
+        abilities.Add(new Ability("Damage", "Increases damage by 10",
                                   gun => ActiveGun._damage += 10,
                                   10));
-        abilities.Add(new Ability("AttackSpeed Boost", "Increases AttackSpeed by 20",
+        abilities.Add(new Ability("AttackSpeed", "Decreases AttackSpeed by 0.05f",
                                   gun => ActiveGun.AttackSpeed -= 0.05f,
                                   10));
+        abilities.Add(new Ability("Multishot", "Increases Multishot by 1",
+                                  gun => ActiveGun.MultiShot += 1,
+                                  10));
     }
-
-
     void ApplyAbility(Ability ability)
     {
         ability.Effect(ActiveGun);
-        Debug.Log(ability.Description);
+    }
+
+
+    void DisplayRandomAbilities()
+    {
+        List<Ability> randomAbilities = GetRandomAbilities(3);
+        foreach (var ability in randomAbilities)
+        {
+            Debug.Log(ability.Name + ": " + ability.Description + "  -  Costs: " + ability.Cost + "hp");
+        }
+    }
+
+    List<Ability> GetRandomAbilities(int count)
+    {
+        return abilities.OrderBy(x => Guid.NewGuid()).Take(count).ToList();
     }
 }
